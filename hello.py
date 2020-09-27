51ffcc842af62b640154cdbbd6b5b026a3c653ea
naoyuki <naoyuki_renesis@mac.com>
2020-09-28 07:25:30 +0900

    文字列囲み文字をシングルクオーツに変更、その他。

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 #この行はコメントです。
-print("Hello Python World!!")
+print('Hello Python World!!')
 #print(1 + 2)
 
 price = 510;
-count = input("数字を入力してください：");
+count = input('数字を入力してください：');
 
 total = price * int(count);
 
-print("合計金額は" + str(total) + "円です。");
+print('合計金額は' + str(total) + '円です。');
 
-testbox = ["apple","orange","banana"];
+testbox = ['apple','orange','banana'];
 
-print(testbox[0]);
+print('1つ目の箱の中に入っているのは' + testbox[0] + 'です。');
