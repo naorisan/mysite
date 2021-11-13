bb6b90238debb0862f429488c2843bb35d9569da
naorisan <naoyuki_renesis@mac.com>
2021-11-14 08:41:38 +0900

    演習続き

--- excel-python.py
+++ excel-python.py
@@ -1,11 +1,19 @@
-goukei = 0
+import pandas as pd #Excelのブックを操作するライブラリ
 
-#iの値を1から1ずつ増やして11になったらループから抜ける
-#for i in range(1,11):
-#for i in range(11):
-for i in range(0, 11, 1):
-    print(i);
-    goukei = goukei + i;
+#Excelに出力するデータを格納するリスト
+item_list = []
+item_no = input("商品番号を入力してください。終了は-1:")
 
-#合計を文字列に変換して表示
-print("合計は" + str(goukei) + "です");
+while(int(item_no) != -1):
+    item_name = input("商品名を入力してください:")
+    item_price = input("価格を入力してください:")
+    #入力したデータをリスト形式で追加
+    item_list.append([item_no, item_name, item_price])
+
+    item_no = input("商品番号を入力してください。終了は-1:")
+
+#見出し（列名）
+df = pd.DataFrame(item_list, columns = ['商品番号', '商品名', '価格'])
+
+with pd.ExcelWriter("sample.xlsx") as writer:
+    df.to_excel(writer, index = False)
